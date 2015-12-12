1ef2105fb21ea95230074d7ad13b442f650e2990
Максим Люборосцев <Mluboroscev@Maksims-MacBook-Pro.local>
2015-12-12 21:40:22 +0300

    Add project

--- scripts/lib/mail.php
+++ scripts/lib/mail.php
@@ -1,28 +1,34 @@
 <?php
 function send_mime_mail(
-                          $name_from,    # имя отправителя
-                          $email_from,   # email отправителя
-                          $name_to,      # имя получателя
-                          $email_to,     # email получателя
-                          $data_charset, # кодировка переданных данных
-                          $send_charset, # кодировка письма
-                          $subject,      # тема письма
-                          $body,         # текст письма
-                          $html          # письмо в виде html или обычного текста
-                        ) {
+  $from,         # отправитель
+  $to,           # получатель
+  $cc,           # копия письма
+  $bcc,          # скрытая копия письма
+  $data_charset, # кодировка переданных данных
+  $send_charset, # кодировка письма
+  $subject,      # тема письма
+  $body,         # текст письма
+  $html          # письмо в виде html или обычного текста
+) {
   
-  $to = mime_header_encode($name_to, $data_charset, $send_charset) . ' <' . $email_to . '>';
+  $from =  mime_header_encode($from['name'], $data_charset, $send_charset) .' <' . $from['email'] . '>';
+  $to = mime_header_encode($to['name'], $data_charset, $send_charset) . ' <' . $to['email'] . '>';
+  
   $subject = mime_header_encode($subject, $data_charset, $send_charset);
-  $from =  mime_header_encode($name_from, $data_charset, $send_charset) .' <' . $email_from . '>';
   
   if($data_charset != $send_charset) {
     $body = iconv($data_charset, $send_charset, $body);
   }
   
-  $headers = "From: $from\r\n";
   $type = ($html) ? 'html' : 'plain';
-  $headers .= "Content-type: text/$type; charset=$send_charset\r\n";
-  $headers .= "Mime-Version: 1.0\r\n";
+  
+  $headers = "Mime-Version: 1.0\r\n";
+  $headers .= "Content-type: text/" . $type . "; charset=" . $send_charset . "\r\n";
+  
+  $headers .= "From: " . $from . "\r\n";
+  
+  if ($cc != '') $headers .= "Cc: " . $cc . "\r\n";
+  if ($bcc != '') $headers .= "Bcc: " . $bcc . "\r\n";
   
   return mail ($to, $subject, $body, $headers);
 
